6ab9bc6756bd90d530ec902fe44f7db557cc2613
jonaprojects <merkava234@gmail.com>
2025-03-18 11:46:09 +0200

    Fixed navbar layout to prevent double vertical scrollbar bug

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,20 +1,11 @@
 "use client";
 
 import { useState } from "react";
-import PrimaryButton from "../buttons/PrimaryButton";
-import classes from "./Navbar.module.css";
 import Link from "next/link";
-
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Menu,
-  MenuItem,
-  Typography,
-} from "@mui/material";
+import { AppBar, Toolbar, IconButton, Menu, MenuItem } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import GitHubButton from "react-github-btn";
+import classes from "./Navbar.module.css";
 
 export default function Navbar() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -30,62 +21,63 @@ export default function Navbar() {
 
   return (
     <nav className={classes.container}>
-      <div className={classes["nav-items"]}>
-        <img src="logo.png" className={classes.logo} alt="logo" />
-        <ul className={classes["nav-links"]}>
-          <li className={classes["nav-link"]}>
-            <Link href="/">Home</Link>
-          </li>
-          <li className={classes["nav-link"]}>
-            <Link href="/">About</Link>
-          </li>
-          <li className={classes["nav-link"]}>
-            <Link href="/background">Background</Link>
-          </li>
-        </ul>
-        <div className={classes["github-buttons"]}>
-          <GitHubButton
-            href="https://github.com/buttons"
-            data-color-scheme="no-preference: light; light: light; dark: dark;"
-            data-size="large"
-            aria-label="Follow @buttons on GitHub"
-          >
-            Follow @buttons
-          </GitHubButton>
-          <GitHubButton
-            href="https://github.com/buttons/github-buttons"
-            data-color-scheme="light"
-            data-icon="octicon-star"
-            data-size="large"
-            aria-label="Star buttons/github-buttons on GitHub"
-          >
-            Star
-          </GitHubButton>
-        </div>
+      {/* Logo */}
+      <img src="logo.png" className={classes.logo} alt="logo" />
 
-        {/* Hamburger Menu Button */}
-        <div className={classes["menu-button"]}>
-          <IconButton color="inherit" aria-label="menu" onClick={handleClick}>
-            <MenuIcon />
-          </IconButton>
-          <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-            <MenuItem onClick={handleClose}>
-              <a href="/" className={classes["menu-link"]}>
-                Home
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <a href="/" className={classes["menu-link"]}>
-                About
-              </a>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <Link href="/background" className={classes["menu-link"]}>
-                Background
-              </Link>
-            </MenuItem>
-          </Menu>
-        </div>
+      {/* Navigation Links */}
+      <ul className={classes["nav-links"]}>
+        <li className={classes["nav-link"]}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={classes["nav-link"]}>
+          <Link href="/">About</Link>
+        </li>
+        <li className={classes["nav-link"]}>
+          <Link href="/background">Background</Link>
+        </li>
+      </ul>
+
+      {/* GitHub Buttons */}
+      <div className={classes['github-buttons']}>
+        <GitHubButton
+          href="https://github.com/buttons"
+          data-size="large"
+          aria-label="Follow @buttons on GitHub"
+        >
+          Follow @buttons
+        </GitHubButton>
+        <GitHubButton
+          href="https://github.com/buttons/github-buttons"
+          data-icon="octicon-star"
+          data-size="large"
+          aria-label="Star buttons/github-buttons on GitHub"
+        >
+          Star
+        </GitHubButton>
+      </div>
+
+      {/* Mobile Menu Button */}
+      <div className={classes['menu-button']}>
+        <IconButton color="inherit" aria-label="menu" onClick={handleClick}>
+          <MenuIcon />
+        </IconButton>
+        <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+          <MenuItem onClick={handleClose}>
+            <Link href="/" className={classes["menu-link"]}>
+              Home
+            </Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link href="/" className={classes["menu-link"]}>
+              About
+            </Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link href="/background" className={classes["menu-link"]}>
+              Background
+            </Link>
+          </MenuItem>
+        </Menu>
       </div>
     </nav>
   );
